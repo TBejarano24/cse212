957922ea424667008697bbb07a6ba26f28306f13
TBejarano24 <bejaranotomas04@gmail.com>
2026-01-09 23:44:04 -0300

    Completed Week01 exercises

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,14 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        var multiples = new double[length]; // First, I create an array with the same capacity as the length specified by the user, which will store all the multiples
+
+        for (int i = 1; i <= length; i++) // Then we loop through each of the numbers from 1 up to the length specified
+        {
+            multiples[i - 1] = number * i; // And from each of those numbers we get the multiples of the specific number up to the specified length, and all the results are stored in the previously created array
+        }
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +36,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // First, I can grab the numbers from the specified index all the way to the end by subtracting the amount to the length of the data, and use the GetRange method to create a new array with the last values of the original array.
+        // With this new array, I can now append to it the starting values of the original array using the AddRange method
+
+        int startIndex = data.Count - amount;
+        List<int> lastValues = data.GetRange(startIndex, amount);
+
+        data.RemoveRange(startIndex, amount);
+        data.InsertRange(0, lastValues);
     }
 }
